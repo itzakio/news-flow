8f9afe4ddeb7d5a3829f6111ef0d1fc61907cf89
itzakio <itzakio3@gmail.com>
2026-01-20 10:31:42 +0600

    project created with besic router setup

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const RootLayout = () => {
+  return (
+    <div className="min-h-100vh flex flex-col">
+      <header>
+        <Navbar />
+      </header>
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default RootLayout;
